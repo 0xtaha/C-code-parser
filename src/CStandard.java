84f224447e36a860670b6d61cb0718aa7478a343
Taha Abdalzez <57445405+0xtaha@users.noreply.github.com>
2021-06-09 16:56:26 +0200

    adding Json file usage

--- src/CStandard.java
+++ src/CStandard.java
@@ -1,15 +1,20 @@
-import java.util.ArrayList;
-import java.util.List;
+import org.json.simple.JSONObject;
 
 public class CStandard extends CBaseVisitor<String> {
+    public JSONObject declarations = new JSONObject();
+    public JSONObject global = new JSONObject();
+    public JSONObject local = new JSONObject();
+
+    CStandard(JSONObject global, JSONObject local, JSONObject declarations){
+        this.global = global;
+        this.local = local;
+        this.declarations = declarations;
+
+    }
 
     public boolean InsideScope  ;
     int Line ;
     int DataStructureType ; // 0 --> Struct , 1 --> Union , 2 --> Enum
-    List<String> StructVar = new ArrayList<>();
-    List<String> UnionVar = new ArrayList<>();
-    List<String> EnumVar = new ArrayList<>();
-
 
 
     public class CListener extends CBaseListener {
@@ -36,15 +41,24 @@ public class CStandard extends CBaseVisitor<String> {
 
     @Override
     public String visitStructOrUnionSpecifier(CParser.StructOrUnionSpecifierContext ctx) {
+        String L_struct_Regex;
+        String L_union_Regex;
+        String struct_Regex;
+        String union_Regex;
+
 
         String val = String.valueOf(ctx.structOrUnion());
 
-        String Dec_structRegex = "([A-Z][a-z0-9]*_)+s" ;
-        String Dec_UnionRegex = "([A-Z][a-z0-9_]*_)+u" ;
+//        String Dec_structRegex = "([A-Z][a-z0-9]*_)+s" ;
+//        String Dec_UnionRegex = "([A-Z][a-z0-9_]*_)+u" ;
+
+        struct_Regex = declarations.get("structures").toString();
+        union_Regex = declarations.get("unions").toString();
 
         if (ctx.structOrUnion().getText().matches("struct")) {
             DataStructureType = 0 ;
-            if ((ctx.Identifier().getText().matches(Dec_structRegex)) == false) {
+//            if ((ctx.Identifier().getText().matches(Dec_structRegex)) == false) {
+            if ((ctx.Identifier().getText().matches(struct_Regex)) == false) {
                 Line = ctx.start.getLine();
                 String L = String.valueOf(Line);
                 System.out.printf("Violation, STRUCT , Line : %d%n", Line);
@@ -53,7 +67,8 @@ public class CStandard extends CBaseVisitor<String> {
 
         else {
             DataStructureType = 1 ;
-            if ((ctx.Identifier().getText().matches((Dec_UnionRegex)) == false)){
+//            if ((ctx.Identifier().getText().matches((Dec_UnionRegex)) == false)){
+            if ((ctx.Identifier().getText().matches((union_Regex)) == false)){
                 Line = ctx.start.getLine();
                 System.out.printf("Violation, UNION , Line : %d%n", Line);
             }
@@ -66,11 +81,16 @@ public class CStandard extends CBaseVisitor<String> {
 
     @Override
     public String visitEnumSpecifier(CParser.EnumSpecifierContext ctx) {
+        String Enum_Regex;
+
         String val = String.valueOf(ctx.Identifier());
-        String EnumRegex = "([A-Z][a-z0-9_]*_)+e";
+        Enum_Regex = declarations.get("enumerations").toString();
+
+//        String EnumRegex = "([A-Z][a-z0-9_]*_)+e";
         DataStructureType = 2 ;
 
-        if ((ctx.Identifier().getText().matches(EnumRegex)) == false) {
+//        if ((ctx.Identifier().getText().matches(EnumRegex)) == false) {
+        if ((ctx.Identifier().getText().matches(Enum_Regex)) == false) {
             Line = ctx.start.getLine();
             System.out.printf("Violation, STRUCT , Line : %d%n", Line);
         }
@@ -79,37 +99,58 @@ public class CStandard extends CBaseVisitor<String> {
 
     @Override
     public String visitDirectDeclarator(CParser.DirectDeclaratorContext ctx) {
+        String func_Regex;
+
         boolean is = InsideScope ;
         String val = String.valueOf(ctx.directDeclarator().Identifier());
+        func_Regex =declarations.get("functions").toString();
 
-        if( (ctx.directDeclarator().Identifier().getText().matches( "([A-Z][A-Za-z0-9]*_)+arg"))){
+//        if( (ctx.directDeclarator().Identifier().getText().matches( "([A-Z][A-Za-z0-9]*_)+arg"))){
+        if( (ctx.directDeclarator().Identifier().getText().matches( func_Regex))){
 
             System.out.println("inside Scope");
         }
         else{
             System.out.println("outside Scope");
         }
-        if ((ctx.directDeclarator().Identifier().getText().matches("([A-Z][a-z0-9]*_)+f")) == false) {
+//        if ((ctx.directDeclarator().Identifier().getText().matches("([A-Z][a-z0-9]*_)+f")) == false) {
+        if ((ctx.directDeclarator().Identifier().getText().matches(func_Regex) == false)) {
             System.out.println("Warning, your variable name violates the correct name for function");
         }
         return val;
     }
 
     @Override public String visitTypedefName(CParser.TypedefNameContext ctx) {
+        String G_struct_Regex;
+        String G_union_Regex;
+        String G_enumerations_Regex;
+        String L_struct_Regex;
+        String L_union_Regex;
+        String L_enumerations_Regex;
+
         String IdString = ctx.Identifier().getText();
         int Line = -1 ;
         boolean is = InsideScope ;
+
+        G_struct_Regex = global.get("structures").toString();
+        G_union_Regex =global.get("unions").toString();
+        G_enumerations_Regex = global.get("enumerations").toString();
+        L_struct_Regex = local.get("structures").toString();
+        L_union_Regex = local.get("unions").toString();
+        L_enumerations_Regex = local.get("enumerations").toString();
 
 
         if(DataStructureType == 0){
             String structRegex ;
             if(is){
                 System.out.println("inside Scope");
-                structRegex = "L_([A-Z][a-z0-9]*_)+s";
+//                structRegex = "L_([A-Z][a-z0-9]*_)+s";
+                structRegex =  L_struct_Regex;
             }
             else{
                 System.out.println("outside Scope");
-                structRegex = "G_([A-Z][a-z0-9]*_)+s";
+//                structRegex = "G_([A-Z][a-z0-9]*_)+s";
+                structRegex = G_struct_Regex;
             }
 
 
@@ -124,12 +165,15 @@ public class CStandard extends CBaseVisitor<String> {
             String UnionRegex ;
             if(is){
                 System.out.println("inside Scope");
-                UnionRegex = "L_([A-Z][a-z0-9_]*_)+u";
+//                UnionRegex = "L_([A-Z][a-z0-9_]*_)+u";
+                UnionRegex = L_union_Regex;
 
             }
             else{
                 System.out.println("outside Scope");
-                UnionRegex = "G_([A-Z][a-z0-9_]*_)+u";
+//                UnionRegex = "G_([A-Z][a-z0-9_]*_)+u";
+                UnionRegex = G_union_Regex;
+
             }
 
 
@@ -143,11 +187,14 @@ public class CStandard extends CBaseVisitor<String> {
             String EnumRegex ;
             if(is){
                 System.out.println("inside Scope");
-                EnumRegex = "L_([A-Z][a-z0-9_]*_)+e";
+//                EnumRegex = "L_([A-Z][a-z0-9_]*_)+e";
+                EnumRegex = L_enumerations_Regex;
+
             }
             else{
                 System.out.println("outside Scope");
-                EnumRegex = "G_([A-Z][a-z0-9_]*_)+e";
+//                EnumRegex = "G_([A-Z][a-z0-9_]*_)+e";
+                EnumRegex = G_enumerations_Regex;
             }
 
             if ((ctx.Identifier().getText().matches(EnumRegex)) == false) {
